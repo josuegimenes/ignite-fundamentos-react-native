728cb19e0e852e990f089fc6923d7163f3720abb
Josué Gimenes <josuegalvares@gmail.com>
2021-10-17 17:03:07 -0300

    Typing Objects and States

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,13 +11,23 @@ import {
 import { Button } from '../components/Button'
 import { SkillCard } from '../components/SkillCard'
 
+interface SkillData {
+  id: string;
+  name: string;
+}
+
 export function Home() {
   const [newSkill, setNewSkill] = useState('')
-  const [mySkills, setMySkills] = useState([])
+  const [mySkills, setMySkills] = useState<SkillData[]>([])
   const [greeting, setGreeting] = useState('')
 
   function handleAddNewSkill() {
-    setMySkills(oldState => [...oldState, newSkill])
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill
+    }
+
+    setMySkills(oldState => [...oldState, data])
   }
 
   useEffect(() => {
@@ -61,9 +71,9 @@ export function Home() {
 
         <FlatList 
           data={mySkills}
-          keyExtractor={item => item}
+          keyExtractor={item => item.id}
           renderItem={({ item }) => (
-            <SkillCard skill={item} />
+            <SkillCard skill={item.name} />
           )}
         />
 
@@ -76,7 +86,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#121015',
-    paddingHorizontal: 20,
     paddingVertical: 70,
     paddingHorizontal: 30
   },
