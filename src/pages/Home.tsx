331f44d2e513fb136a8baf5e0eeef0fe4b6f2957
Josué Gimenes <josuegalvares@gmail.com>
2021-10-17 18:01:46 -0300

    Typing Components with TypeScript

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -63,7 +63,10 @@ export function Home() {
           onChangeText={setNewSkill}
         />
 
-        <Button onPress={handleAddNewSkill} />         
+        <Button 
+          title="Add"
+          onPress={handleAddNewSkill}
+        />
         
         <Text style={[styles.title, { marginVertical: 50 }]}>
           My Skills
